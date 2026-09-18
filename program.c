869badea90b38422da65c90e728b17a42a518cf3
Matys787 <matysetelin787@gmail.com>
2026-09-18 08:54:03 +0000

    modification variable

--- program.c
+++ program.c
@@ -5,8 +5,8 @@ int main() {
     int alt = 10000; 
     int cap = 230;
     int QNH = 1013;
-    int Radio = 108.70;
-    int ILS = 8.6;
+    double Radio = 108.70;
+    double ILS = 8.6;
 
     /*Echelle Speed*/
     int speed1 = 260 - 40;
@@ -25,19 +25,19 @@ int main() {
     printf("|    | THR CLB|     |     | DUAL | AP1+2 |   |       |\n");
     printf("|                                            | 6.    |\n");
     printf("|    __kts__       .   °   .       ___ft___  | -  |  |\n");
-    printf("|   |     _|    °______^_____ °   |  115_|   | 2. |  |\n", alt4);
+    printf("|   |     _|    °______^_____ °   |  %5d_|   | 2. |  |\n", alt4);
     printf("|   |  %4d_|    /     __        ° |     _|   | -  |  |\n", speed4);
-    printf("|   |     _|   |   10____10   |   |  110_|___| 1. |  |\n", alt3);
+    printf("|   |     _|   |   10____10   |   |  %5d_|___| 1. |  |\n", alt3);
     printf("|   |  %3d_|   |----] __ [----| ° |     _|20 | - <|  |\n", speed3);
-    printf("|   |     <|   |______________| ^ |   10800  |__  |  |\n", speed, alt);
+    printf("|   |  %2d_|   |______________| ^ |   10800  |__  |  |\n", speed, alt);
     printf("|   |  %2d_|   |      __      | ° |     _|80 |    |  |\n", speed2);
-    printf("|   |     _|   |   10____10   |   |  105_|___| -  |  |\n", alt2);
+    printf("|   |     _|   |   10____10   |   |  %5d_|___| -  |  |\n", alt2);
     printf("|   |  %1d_|    __20______20__/ ° |     _|   | 1. |  |\n", speed1);
     printf("|   |______|_                     |______|_  | -  |  |\n", alt1);
     printf("|             °   °  <|>  °   °              | 2. |  |\n");
     printf("|  IMNW    ________________________ QNH %d   | -  |  |\n", QNH);
-    printf("| Radio %d|   |   |   | ^ |   |   |          | 6. /  |\n", Radio);
-    printf("| ILS %dNM|___24______23_____22___|          |___/   |\n", ILS);
+    printf("| Radio %.2f|   |   |   | ^ |   |   |          | 6. /  |\n", Radio);
+    printf("| ILS %.1fNM|___24______23_____22___|          |___/   |\n", ILS);
     printf("|____________________________________________________|\n");
 
    return 0;
