239619f0701b358648ddb2aa451dc1900c453697
Matys787 <matysetelin787@gmail.com>
2026-09-18 09:49:21 +0000

    modification

--- program.c
+++ program.c
@@ -1,55 +1,52 @@
 #include <stdio.h>
 int main() {
+
     /*Valeurs des variables*/
-    int speed = 260;
-    int alt = 10000; 
+
     int cap = 230;
-    %4.f = 230;
     int QNH = 1013;
-    %.3f = QNH;
-    double Radio = (108.00 + 111.95) / 2;
-    %.2f = Radio;
-    double ILS = 8;
-    %.1f = ILS;
+    int ILS = 8;
 
     /*Echelle Speed*/
+    int speed = 260;
     int speed1 = 260 - 40;
-    %1d = speed1;
+    %1d = 220
     int speed2 = 260 - 20;
-    %2d = speed2;
+    %2d = 240
     int speed3 = 260 + 20;
-    %3d = speed3;
+    %3d = 280
     int speed4 = 260 + 40;
-    %4d = speed4;
+    %4d = 300
 
     /* Echelle indicateur alt*/
-    int alt5 = 10000 - 1000;
-    %5d = alt5;
-    int alt6 = 10000 - 500;
-    %6d = alt6;
-    int alt7 = 10000 + 500;
-    %7d = alt7;
-    int alt8 = 10000 + 1000;
-    %8d = alt8;
+    int altitude = 10000;
+    int altitude5 = altitude - 1000;
+    
+    int altitude6 = altitude - 500;
+   
+    int altitude7 = altitude + 500;
+  
+    int altitude8 = altitude + 1000;
+   
 
     printf(" ____________________________________________________ \n");
     printf("|    |  SPEED | G/S | LOC | CAT3 | A/THR |    ___    |\n");   
-    printf("|    | THR CLB|     |     | DUAL | AP1+2 |   |       |\n");
-    printf("|                                            | 6.    |\n");
+    printf("|    | THR CLB|     |     | DUAL | AP1+2 |   |   \   |\n");
+    printf("|                                            | 6. \  |\n");
     printf("|    __kts__       .   °   .       ___ft___  | -  |  |\n");
-    printf("|   |     _|    °______^_____ °   |  %8d_|   | 2. |  |\n");
-    printf("|   |  %4d_|    /     __        ° |     _|   | -  |  |\n");
-    printf("|   |     _|   |   10____10   |   |  %7d_|___| 1. |  |\n");
-    printf("|   |  %3d_|   |----] __ [----| ° |     _|20 | - <|  |\n");
-    printf("|   |  %2d_|   |______________| ^ |   10000  |__  |  |\n");
-    printf("|   |  %2d_|   |      __      | ° |     _|80 |    |  |\n");
-    printf("|   |     _|   |   10____10   |   |  %6d_|___| -  |  |\n");
-    printf("|   |  %1d_|    __20______20__/ ° |     _|   | 1. |  |\n");
-    printf("|   |______|_                     |__%5d_|_  | -  |  |\n");
+    printf("|   |     _|    °______^______ °  |  %6d_|   | 2. |  |\n", altitude8);
+    printf("|   |  %d_|   /      __      \ ° |     _|   | -  |  |\n");
+    printf("|   |     _|   |   10____10   |   |  %6d_|___| 1. |  |\n", altitude7);
+    printf("|   |  %d_|   |----] __ [----| ° |     _|20 | - <|  |\n");
+    printf("|   |    <_|   |______________| ^ |  %6d     |    |  |\n", altitude);
+    printf("|   |  %d_|   |      __      | ° |     _|80 |    |  |\n");
+    printf("|   |     _|   |   10____10   |   |  %6d_|___| -  |  |\n", altitude6);
+    printf("|   |  %d_|   \__20______20__/ ° |     _|   | 1. |  |\n");
+    printf("|   |______|_                     |__%6d_|_  | -  |  |\n", altitude5);
     printf("|             °   °  <|>  °   °              | 2. |  |\n");
     printf("|  IMNW    ________________________ QNH %.3f | -  |  |\n");
     printf("|Radio %.2f|   |   |   | ^ |   |   |         | 6. /  |\n");
-    printf("|ILS %.1fNM|___24____%4.f______22___|         |___/  |\n");
+    printf("|ILS %.1fNM|___24_____23______22___|         |___/   |\n");
     printf("|____________________________________________________|\n");
 
    return 0;
